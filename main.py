4534f1b4f1bc0dda8cafbbca60ce170641d8ba2c
dhruv50ae <linkdp2012@gmail.com>
2022-04-14 14:46:28 +0100

    Endgame Commit

--- main.py
+++ main.py
@@ -1,48 +1,81 @@
 
+from turtle import clear
 import random
+
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
 
 def dealCard():
-    """Function to deal a card"""
+    """Returns a random card from the deck."""
     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
     card = random.choice(cards)
     return card
 
 
 def calculateScore(cards):
+    """Take a list of cards and return the score calculated from the cards"""
+
     if sum(cards) == 21 and len(cards) == 2:
         return 0
-    if 11 in cards and sum(cards) > 2:
+    if 11 in cards and sum(cards) > 21:
         cards.remove(11)
         cards.append(1)
     return sum(cards)
 
 
-userCards = []
-computerCards = []
-isGameOver = False
-
-for _ in range(2):
-    userCards.append(dealCard())
-    computerCards.append(dealCard())
-
-while not isGameOver:
-
-    userScore = calculateScore(userCards)
-    computerScore = calculateScore(computerCards)
-    print(f"Your cards: {userCards}, current score: {userScore}")
-    print(f"Computer's first card: {computerCards[0]}")
-
-    if userScore == 0 or computerScore == 0 or userScore > 21:
-        isGameOver = True
+def compare(userScore, computerScore):
+    if userScore == computerScore:
+        return "Draw"
+    elif computerScore == 0:
+        return "Lose, opponent has Blackjack"
+    elif userScore == 0:
+        return "Win with a Blackjack"
+    elif userScore > 21:
+        return "You went over. You lose"
+    elif computerScore > 21:
+        return "Opponent went over. You win"
+    elif userScore > computerScore:
+        return "You win"
     else:
-        userShouldDeal = input(
-            "Type 'y' to get another card, type 'n' to pass: ")
-        if userShouldDeal == "y":
-            userCards.append(dealCard())
-        else:
-            isGameOver = True
+        return "You lose"
 
-while computerScore != 0 and computerScore < 17:
-    computerCards.append(dealCard())
-    computerScore = calculateScore(computerCards)
+
+def playGame():
+
+    userCards = []
+    computerCards = []
+    isGameOver = False
+
+    for _ in range(2):
+        userCards.append(dealCard())
+        computerCards.append(dealCard())
+
+    while not isGameOver:
+        userScore = calculateScore(userCards)
+        computerScore = calculateScore(computerCards)
+        print(f"   Your cards: {userCards}, current score: {userScore}")
+        print(f"   Computer's first card: {computerCards[0]}")
+
+        if userScore == 0 or computerScore == 0 or userScore > 21:
+            isGameOver = True
+        else:
+            userShould_deal = input(
+                "Type 'y' to get another card, type 'n' to pass: ")
+            if userShould_deal == "y":
+                userCards.append(dealCard())
+            else:
+                isGameOver = True
+
+    while computerScore != 0 and computerScore < 17:
+        computerCards.append(dealCard())
+        computerScore = calculateScore(computerCards)
+
+    print(f"   Your final hand: {userCards}, final score: {userScore}")
+    print(
+        f"   Computer's final hand: {computerCards}, final score: {computerScore}")
+    print(compare(userScore, computerScore))
+
+
+while input("Do you want to play a game of Blackjack? Type 'y' or 'n': ") == "y":
+    clear()
+    playGame()
