4af3f6cb18a7a518d3d5f1119fe27cc48d028d24
dhruv50ae <linkdp2012@gmail.com>
2022-04-14 13:21:29 +0100

    Created the deal card function

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 
-cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+import random
+
+
+def dealCard():
+    """Function to deal a card"""
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    card = random.choice(cards)
+    return card
+
+
+userCard = []
+computerCard = []
+
+for _ in range(2):
+    userCard.append(dealCard())
+    computerCard.append(dealCard())
